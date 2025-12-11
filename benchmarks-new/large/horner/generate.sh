94ea775a6922027188dd4b042832dbf3e217cb25
Max Fan <root@max.fan>
2025-12-11 19:57:43 +0000

    Update generation script

--- benchmarks-new/large/horner/generate.sh
+++ benchmarks-new/large/horner/generate.sh
@@ -1,7 +1,7 @@
 generate() {
-    python3 generate_horner_fptaylor.py $i > Horner$i.fptaylor
-    python3 generate_horner_fz.py $i > Horner$i.fz
-    python3 generate_horner_g.py $i > Horner$i.g
+    python3 generate_horner_fptaylor.py $1 Horner$1.fptaylor
+    python3 generate_horner_fz.py $1 Horner$1.fz
+    python3 generate_horner_g.py $1 Horner$1.g
 }
 
 generate 5
@@ -10,5 +10,5 @@ generate 20
 generate 50
 
 for i in {100..2000..100}; do
-    generate i
+    generate $i
 done
