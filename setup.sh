c0b9775ebbf33d8cee0bcc0104a9503a5704b2e9
Max Fan <root@max.fan>
2026-01-07 19:24:16 -0500

    Add deps to setup.sh script

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ set -exuo pipefail
 [ ! -d ".venv" ] && uv venv .venv
 source .venv/bin/activate
 uv pip install -e fpcodgen/
+uv pip install numpy pandas seaborn tqdm
 git submodule update --init --recursive
 cd fuzzrs && cargo build --release && cd ..
 sudo docker build -f docker/Dockerfile -t negfuzz .
