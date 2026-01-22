e0a435cc116c8ce9e38e969b58422fd15cc281e4
Max Fan <root@max.fan>
2026-01-22 16:15:51 +0000

    Run Satire the same way they do in the paper

--- bench_large.sh
+++ bench_large.sh
@@ -85,7 +85,7 @@ SATIRE_FILE="benchmarks-new/$BASE_NAME.txt"
 
 # No abstraction - with dynamic LD_LIBRARY_PATH
 run_in_docker "bash -c 'cd /numerics-playground/deps/Satire && \
-  python3 src/satire.py --std --file ../../${SATIRE_FILE} \
+  python3 src/satire.py --std --compress --file ../../${SATIRE_FILE} \
   --logfile ../../benchmarks-new/${BASE_NAME}_sat_abs-serial_noAbs.pylog \
   --outfile ../../benchmarks-new/${BASE_NAME}_sat_abs-serial_noAbs.out'"
 
@@ -93,7 +93,7 @@ run_in_docker "bash -c 'cd /numerics-playground/deps/Satire && \
 for config in "10 20" "15 25" "20 40"; do
   read -r mindepth maxdepth <<< "$config"
   run_in_docker "bash -c 'cd /numerics-playground/deps/Satire && \
-    python3 src/satire.py --std --file ../../${SATIRE_FILE} \
+    python3 src/satire.py --std --compress --file ../../${SATIRE_FILE} \
     --enable-abstraction --mindepth '"${mindepth}"' --maxdepth '"${maxdepth}"' \
     --logfile ../../benchmarks-new/${BASE_NAME}_sat_abs-serial_'"${mindepth}"'_'"${maxdepth}"'.pylog \
     --outfile ../../benchmarks-new/${BASE_NAME}_sat_abs-serial_'"${mindepth}"'_'"${maxdepth}"'.out'"
